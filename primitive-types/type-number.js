a418cffa676e86babb538b1564f7f8504935c1d3
Agatha França <agathafranca@outlook.com>
2022-01-13 18:47:56 -0300

    Using the number type

---
+++ primitive-types/type-number.js
@@ -0,0 +1,15 @@
+const meuNumero = 5;
+
+const primeiroNumero = 10;
+const segundoNumero = 4;
+
+const operacaoMatematica = primeiroNumero / segundoNumero;
+
+console.log(operacaoMatematica);
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao);
