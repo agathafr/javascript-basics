ee2e3016a540c4fb0761ea841621b49e6617e58e
Agatha França <agathafranca@outlook.com>
2022-01-13 19:00:09 -0300

    Using the string type

---
+++ primitive-types/type-string.js
@@ -0,0 +1,11 @@
+const textoUm = "Olá, mundo!";
+const textoDois = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const stringDeNumeros = "123456";
+
+const frase = 'Meu nome é';
+const meuNome = 'Agatha';
+
+const citacao = 'O Léo disse: "Oi"';
+
+console.log(citacao + meuNome);
