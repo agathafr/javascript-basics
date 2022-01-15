15e46fbd1aa1957fda0fc06b74cb6863f2dbbbb3
Agatha França <agathafranca@outlook.com>
2022-01-15 09:14:18 -0300

    Exemplifies data types

---
+++ declaring-variables/truthy-falsy.js
@@ -0,0 +1,12 @@
+const usarioLogado = true
+const contapaga = false
+
+console.log(0 == false)
+console.log("" == false)
+console.log(1 == true)
+
+let minhaVar;
+let varNull = null;
+
+console.log(typeof minhaVar)
+console.log(typeof varNull)
