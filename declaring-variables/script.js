d9901e83e6a411ccef7d9aa296c72b27883efb29
Agatha França <agathafranca@outlook.com>
2022-01-15 09:01:28 -0300

    Exemplifies the declaration of variables

---
+++ declaring-variables/script.js
@@ -0,0 +1,24 @@
+var altura = 5;
+var comprimento = 7;
+
+var area = altura * comprimento;
+
+let alturaLet = 5;
+let comprimentoLet = 6;
+
+let areaLet = alturaLet * comprimentoLet;
+
+console.log(areaLet)
+
+let forma = 'retângulo';
+
+if (forma === 'retângulo') {
+    area = altura * comprimento;
+} else {
+    area = (altura * comprimento) / 2;
+}
+
+console.log(area)
+
+
+
